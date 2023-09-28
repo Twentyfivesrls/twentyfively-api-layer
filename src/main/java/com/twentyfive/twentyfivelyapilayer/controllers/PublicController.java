a331afd1a5195cee38156031167787641a6ddc6b
twentyfive <twentyfivesrls@gmail.com>
2023-09-28 13:29:55 +0200

    test controller

--- src/main/java/com/twentyfive/twentyfivelyapilayer/controllers/PublicController.java
+++ src/main/java/com/twentyfive/twentyfivelyapilayer/controllers/PublicController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/public")
 @CrossOrigin(origins = "*")
-public class TestController {
+public class PublicController {
     @RequestMapping("/hello")
     public String hello(){
         return "Hello World";
